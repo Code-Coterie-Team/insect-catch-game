f31e0b263800a6fec88b7250a7f961e2cf1a9aa3
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-10-02 18:25:43 +0330

    add go to the next page

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 const screens = document.querySelectorAll('.screen');
 const playBtn = document.querySelector(".playBtn");
+const favInscetBtn = document.querySelector('.chooseInsectBtn')
 
 function startGame(){
     screens[0].classList.add('up');
+}
+
+function chooseFavInsect(){
+    screens[1].classList.add('up')
 }
