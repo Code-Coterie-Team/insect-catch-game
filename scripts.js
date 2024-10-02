28a169ccefeffb8e22dde040d018e5b9189ccad0
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-10-02 19:00:08 +0330

    add timer

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const screens = document.querySelectorAll('.screen');
 const playBtn = document.querySelector(".playBtn");
-const favInscetBtn = document.querySelector('.chooseInsectBtn')
+const favInscetBtn = document.querySelector('.chooseInsectBtn');
+const timer = document.querySelector('.timer');
+let seconds = 0;
 
 function startGame(){
     screens[0].classList.add('up');
@@ -8,4 +10,15 @@ function startGame(){
 
 function chooseFavInsect(){
     screens[1].classList.add('up')
+    setInterval(increaseTime, 1000);
+}
+
+function increaseTime() {
+    console.log("salam")
+    let m = Math.floor(seconds / 60)
+    let s = seconds % 60
+    m = m < 10 ? `0${m}` : m
+    s = s < 10 ? `0${s}` : s
+    timer.innerHTML = `Time: ${m}:${s}`
+    seconds++
 }
